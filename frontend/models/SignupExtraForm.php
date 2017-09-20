848f99091068541d7e2896b53340689e702c10fb
maxym16 <goolyamaxym@gmail.com>
2017-09-20 15:28:31 +0300

    Add SingupExtra

--- frontend/models/SignupExtraForm.php
+++ frontend/models/SignupExtraForm.php
@@ -13,6 +13,18 @@ use common\models\UserProfileRole;
 class SignupExtraForm extends Model
 {
     public $nome;
+    public $cognome;
+    public $sesso;
+    public $user_profile_age_group_id;
+    public $telefono;
+    public $facebook;
+    public $googleplus;
+    public $linkedin;
+    public $avatar_id;
+    public $note;
+    public $user_profile_role_id;
+    public $presentazione_breve;
+    public $presentazione_personale;
     public $email;
     public $password;
 
@@ -23,13 +35,21 @@ class SignupExtraForm extends Model
     public function rules()
     {
         return [
-            ['nome', 'trim'],
-            ['nome', 'required'],
-            ['nome', 'unique', 'targetClass' => '\common\models\UserProfile', 'message' => 'This username has already been taken.'],
-            ['nome', 'string', 'min' => 2, 'max' => 255],
+//            ['nome', 'trim'],
+//            ['nome', 'required'],
+//            ['nome', 'unique', 'targetClass' => '\common\models\UserProfile', 'message' => 'This username has already been taken.'],
+//            ['nome', 'string', 'min' => 2, 'max' => 255],
+//            ['cognome', 'trim'],
+//            ['cognome', 'required'],
+//            ['cognome', 'unique', 'targetClass' => '\common\models\UserProfile', 'message' => 'This username has already been taken.'],
+//            ['cognome', 'string', 'min' => 2, 'max' => 255],
 
+            [['nome', 'cognome'], 'required'],
+            [['sesso', 'presentazione_personale', 'note'], 'string'],
+            [['avatar_id', 'user_profile_role_id', 'user_profile_age_group_id'/*, 'user_id'*/], 'integer'],
+            [['nome', 'cognome', 'presentazione_breve', 'telefono'/*, 'status'*/, 'facebook', 'linkedin', 'googleplus'], 'string', 'max' => 255],
+            
             ['email', 'trim'],
-            ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
@@ -53,6 +73,8 @@ class SignupExtraForm extends Model
         $user = new User();
         $userpr = new UserProfile();
         $userpr->nome = $this->nome;
+        $userpr->cognome = $this->cognome;
+        $userpr->sesso = $this->sesso;
         $user->email = $this->email;
         $user->setPassword($this->password);
         $user->generateAuthKey();
--- frontend/views/site/signup-extra.php
+++ frontend/views/site/signup-extra.php
@@ -8,13 +8,28 @@ use yii\widgets\ActiveForm;
 /* @var $form ActiveForm */
 //var_dump($model);
 //echo '<pre>' . print_r($model, true) . '</pre>';
-debug($model);
+//debug($model);
 ?>
 <div class="site-signup-extra">
 
     <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'nome') ?>
+        <?= $form->field($model, 'nome')->label('Name') ?>
+        <?= $form->field($model, 'cognome')->label('Surname') ?>
+        <?= $form->field($model, 'sesso')->label('Sex')->listBox(['1'=>'male','2'=>'female']) ?>
+        <?= $form->field($model, 'user_profile_age_group_id')->label('Age')->dropDownList(['1'=>'18-25','2'=>'26-35','3'=>'36-45','4'=>'46-55','5'=>'56-65','6'=>'>65']) ?>
+        <?= $form->field($model, 'telefono')->label('Phone') ?>
+        <?= $form->field($model, 'facebook')->label('facebook') ?>
+        <?= $form->field($model, 'googleplus')->label('googleplus') ?>
+        <?= $form->field($model, 'linkedin')->label('linkedin') ?>
+        <?= $form->field($model, 'avatar_id')->label('Photo') ?>
+        <?= $form->field($model, 'note')->label('About me')->textarea(['rows'=>5]) ?>
+        <?= $form->field($model, 'user_profile_role_id')->label('Role')->dropDownList(['1'=>'fan','2'=>'contributor','3'=>'partner','4'=>'sponsor']) ?>
+        <?= $form->field($model, 'presentazione_breve')->label('Remote work')->dropDownList(['1'=>'Yes','2'=>'No','3'=>'Maybe']) ?>
+        <?= $form->field($model, 'presentazione_personale')->label('Effort')->dropDownList(['1'=>'full-time','2'=>'part time','3'=>'occasional']) ?>
+
+    <!--    < ? = $form->field($model, 'email')->label('Change email') ? >
+        < ? = $form->field($model, 'password')->label('Change password')->passwordInput() ? >-->
     
         <div class="form-group">
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
